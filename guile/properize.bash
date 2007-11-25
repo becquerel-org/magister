63f2585b68edbf9d6806dac1319ac4c59b028ad6
Leonardo Valeri Manera <lvalerimanera@gmail.com>
2007-11-25 18:55:33 +0100

    (properize): moved to project root dir.

--- guile/properize.bash
+++
@@ -1,7 +0,0 @@
-#
-# properize.bash
-#
-# This is the hook used by properize to implement atomic resume
-# of installation consistency operations.
-#
-STATE_DIR=$(guile -l /etc/properize.conf -c '(display *state-dir*)')
