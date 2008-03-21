04fafc8c8b2db3490cb164dc62858efa55b46e56
Leonardo Valeri Manera <lvalerimanera@gmail.com>
2008-03-21 02:39:51 +0100

    (parse-commandline): start conversion

--- magister.scm
+++ magister.scm
@@ -352,7 +352,6 @@ will be installed. Then system (if you asked for it) will be rebuilt.\n\n"))
 ;;; Action-list generation
 ;; {{{ (generate-toolchain-list): Creates a list of toolchain packages to be reinstalled.
 ;; linux-headers glibc libtool binutils (gmp mpfr) gcc ?libstdc++-v3 ?gcc:3.3
-;; (grep use-flag (read-pipe-line (conc "paludis --environment-variable " package-spec " USE"))
 (define (generate-toolchain-list)
   (let* ([package-table (make-hash-table)]
 	 [toolchain-list (list (extract-package "linux-headers"))]
@@ -497,6 +496,63 @@ will be installed. Then system (if you asked for it) will be rebuilt.\n\n"))
 
 ;;; Command-line option parser
 ;; {{{ (parse-commandline): Parses commandline using (args).
+(define (parse-commandline)
+  (let ([option-spec   (list (args:make-option (p pretend)              #:none
+			  "Pretend only: do not reinstall")
+                             (args:make-option (v verbose)              #:none
+			  "Be verbose about what's going on\n")
+                             (args:make-option (t toolchain)            #:none
+			  "Reinstall the toolchain")
+                             (args:make-option (s system)               #:none
+			  "Reinstall the 'system' set
+                                     Toolchain packages are filtered out")
+                             (args:make-option (e everything)           #:none
+			  "Reinstall the 'everything' set
+                                     Toolchain and 'system' packages are filtered out\n")
+                             (args:make-option (version-lock)          (#:required "level")
+			  "How specific to be about the package's version
+                     none            Only use the package category/name
+                     slot            Use slot information where appropriate (default)
+                     version         Use the version number")
+                             (args:make-option (dl-installed-deps-pre) (#:required "option")
+			  "As per the paludis option")
+                             (args:make-option (checks)                (#:required "when")
+			  "As per the paludis option, defaults to 'none'")
+                             (args:make-option (debug-build)           (#:required "option")
+			  "As per the paludis option, defaults to 'none'\n")
+                             (args:make-option (r resume)               #:none
+			  "Resume an aborted reinstallation\n\n")
+                             (args:make-option (V version)              #:none
+			  "Print version and exit"
+			  (print "args-test v0.0.1")
+			  (exit))
+                             (args:make-option (h help)                 #:none
+			  "Display this text"
+			  (usage)))]
+        [usage (lambda ()
+                 (with-output-to-port (current-output-port)
+                   (lambda ()
+                     (print "Usage: magister [options...]")
+                     (print "Rebuild all installed packages, or parts thereof.")
+                     (newline)
+                     (print (parameterize ((args:separator ", ")
+                                           (args:indent 2)
+                                           (args:width 35))
+                              (args:usage options)))
+                     (newline)
+                     (print "Examples:
+  magister -t                        Rebuild the toolchain.
+  magister -ts --version-lock=none   Rebuild toolchain and system,
+                                     doing all possible upgrades.")
+                     (newline)
+                     (print "Report bugs to lvalerimanera at gmail.")))
+                 (exit))])
+    (receive (options operands)
+        (args:parse (command-line-arguments) options)
+      (print "--version-lock -> " (alist-ref 'version-lock options))
+      (print options)
+      (print operands))))
+    
 (define (parse-commandline)
   (let* ((option-spec `((help (single-char #\h) (value #f))
 			(version (single-char #\v) (value #f))
